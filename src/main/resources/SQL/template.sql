279ee9aecb7bbf7883035622fcfc931615878940
Milan Sawicki <milan.sawicki@student.put.edu.pl>
2017-01-30 18:16:02 +0100

    enum fix

--- src/main/resources/SQL/template.sql
+++ src/main/resources/SQL/template.sql
@@ -3,13 +3,14 @@ INSERT INTO restaurant.stand (id, table_number) VALUES (1,1);
 INSERT INTO restaurant.stand (id, table_number) VALUES (2,2);
 INSERT INTO restaurant.stand (id, table_number) VALUES (3,3);
 INSERT INTO restaurant.stand (id, table_number) VALUES (4,4);
+
 -- PRODUCT TYPE --
-INSERT INTO restaurant.product(product_id, name, price, prod_type) VALUES (1, 'Tea', 5.5, 0);
-INSERT INTO restaurant.product(product_id, name, price, prod_type) VALUES (2, 'Water', 5, 0);
-INSERT INTO restaurant.product(product_id, name, price, prod_type) VALUES (3, 'Beer', 10, 0);
-INSERT INTO restaurant.product(product_id, name, price, prod_type) VALUES (4, 'Scrambled Eggs', 10, 1);
-INSERT INTO restaurant.product(product_id, name, price, prod_type) VALUES (5, 'Sandwich', 10, 1);
-INSERT INTO restaurant.product(product_id, name, price, prod_type) VALUES (6, 'Tomato Soup', 10, 1);
+INSERT INTO restaurant.product(product_id, name, price, product_type) VALUES (1, 'Tea', 5.5, 0);
+INSERT INTO restaurant.product(product_id, name, price, product_type) VALUES (2, 'Water', 5, 0);
+INSERT INTO restaurant.product(product_id, name, price, product_type) VALUES (3, 'Beer', 10, 0);
+INSERT INTO restaurant.product(product_id, name, price, product_type) VALUES (4, 'Scrambled Eggs', 10, 1);
+INSERT INTO restaurant.product(product_id, name, price, product_type) VALUES (5, 'Sandwich', 10, 1);
+INSERT INTO restaurant.product(product_id, name, price, product_type) VALUES (6, 'Tomato Soup', 10, 1);
 
 -- WAITER --
 INSERT INTO restaurant.waiter(id, name, surname) VALUES (1, 'Milan', 'Sawicki');
